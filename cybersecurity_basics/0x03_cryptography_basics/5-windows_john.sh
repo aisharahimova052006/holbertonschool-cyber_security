bd1afe06a887a7db7096e3af24606c5aa6f354c1
Aysha Rahimli <aisharahimova052006@gmail.com>
2026-02-22 06:40:29 -0500

    cryptography late tasks

--- cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist rockyou.txt --format=nt "$1" ; john --show --format=nt "$1"| awk -F: '/:/ {print $2}' | head -n 1 >5_password.txt
+john --wordlist /usr/share/wordlists/rockyou.txt --format=nt "$1" ; john --show --format=nt "$1"| awk -F: '/:/ {print $2}' | head -n 1 >5-password.txt
