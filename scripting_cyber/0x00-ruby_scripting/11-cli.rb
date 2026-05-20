eb8621def378158c277bc2c180ea905a4eef3f9d
Aysha Rahimli <aisharahimova052006@gmail.com>
2026-05-20 11:27:29 -0400

    scripting cyber

---
+++ scripting_cyber/0x00-ruby_scripting/11-cli.rb
@@ -0,0 +1,60 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
+
+FILE = "tasks.txt"
+
+def load_tasks
+  return [] unless File.exist?(FILE)
+  File.readlines(FILE, chomp: true)
+end
+
+def save_tasks(tasks)
+  File.open(FILE, "w") do |f|
+    tasks.each { |t| f.puts(t) }
+  end
+end
+
+options = {}
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: cli.rb [options]"
+
+  opts.on("-aTASK", "--add TASK", "Add a new task") do |task|
+    options[:add] = task
+  end
+
+  opts.on("-l", "--list", "List all tasks") do
+    options[:list] = true
+  end
+
+  opts.on("-rINDEX", "--remove INDEX", "Remove a task by index") do |index|
+    options[:remove] = index.to_i
+  end
+
+  opts.on("-h", "--help", "Show help") do
+    puts opts
+    exit
+  end
+end.parse!
+
+tasks = load_tasks
+
+if options[:add]
+  tasks << options[:add]
+  save_tasks(tasks)
+  puts "Task '#{options[:add]}' added."
+
+elsif options[:list]
+  tasks.each_with_index do |task, i|
+    puts "#{i + 1}. #{task}"
+  end
+
+elsif options[:remove]
+  index = options[:remove] - 1
+  if index >= 0 && index < tasks.length
+    removed = tasks.delete_at(index)
+    save_tasks(tasks)
+    puts "Task '#{removed}' removed."
+  end
+end
