e822e2e198afab23bfcfa7ef59859441ef0010fc
Aysha Rahimli <aisharahimova052006@gmail.com>
2026-05-20 11:04:32 -0400

    scripting cyber

--- scripting_cyber/0x00-ruby_scripting/1-main.rb
+++ scripting_cyber/0x00-ruby_scripting/1-main.rb
@@ -1,5 +1,9 @@
-#!/usr/bin/env ruby
-require_relative '1-hello_world_class'
+require_relative '3-read_file'
+require_relative '4-write_file'
 
-hello_world_instance = HelloWorld.new
-hello_world_instance.print_hello
+file1_path = 'file2.json'
+file2_path = 'file.json'
+merge_json_files(file1_path, file2_path)
+puts 'File merged successfully!'
+
+count_user_ids(file2_path)
