939c872a756ab5416e3ea6072e63552d7b217cf5
Aysha Rahimli <aisharahimova052006@gmail.com>
2026-05-20 10:56:23 -0400

    scripting cyber

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 require 'json'
+require_relative '3-read_file'
 
 def write_file(file_path, data)
   File.open(file_path, 'w') do |file|
@@ -10,6 +11,10 @@ end
 def merge_json_files(file1_path, file2_path)
   data1 = read_file(file1_path)
   data2 = read_file(file2_path)
+
   merged = data2.merge(data1)
+
   write_file(file2_path, merged)
+
+  puts "Merged JSON written to #{file2_path}"
 end
